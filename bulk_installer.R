d336eabfdfba67f9745e9f529d95dde5d053c83a
aaronpolhamus <aaron@credijusto.com>
2018-06-09 19:24:34 -0500

    Update bulk_installer.R

--- bulk_installer.R
+++ bulk_installer.R
@@ -4,5 +4,5 @@ packages_to_install <- dir(PACKAGE_DIRECTORY)
 for(package in packages_to_install){
   package_path <- file.path(PACKAGE_DIRECTORY, package)
   print(sprintf("Installing %s using this as the full path: %s", package, package_path))
-  install.packages(package_path, repos = NULL, type="source")
+  install.packages(package_path, repos=NULL, type="source")
 }
